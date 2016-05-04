7f46e9fb9e39dccc03b4d3a66aa8d8cdd2596127
Miroslav Batchkarov <mbatchkarov@gmail.com>
2016-05-04 12:15:20 +0100

    v1 of http consumer- can read stuff, but does not respond (writes to file)

---
+++ baleen/baleen-collectionreaders/src/main/java/uk/gov/dstl/baleen/collectionreaders/HttpReader.java
@@ -0,0 +1,111 @@
+package uk.gov.dstl.baleen.collectionreaders;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.DocumentAnnotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ConcurrentArrayQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.dstl.baleen.uima.BaleenCollectionReader;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Queue;
+
+/**
+ * Created by mmb28 on 04/05/2016.
+ */
+public class HttpReader extends BaleenCollectionReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpReader.class);
+    Queue<Document> data;
+    Document nextDocument;
+
+    private Server server;
+    private ServletContextHandler servletContextHandler;
+
+    @Override
+    protected void doInitialize(UimaContext uimaContext) throws ResourceInitializationException {
+        data = new ConcurrentArrayQueue<>();
+
+        this.server = new Server(InetSocketAddress.createUnresolved("0.0.0.0", 3124));
+        servletContextHandler = new ServletContextHandler();
+        servletContextHandler.setContextPath("/sussex");
+
+        servletContextHandler.addServlet(new ServletHolder(new MyServlet(this)), "/consume/*");
+        server.setHandler(servletContextHandler);
+
+        if (this.server != null) {
+            LOGGER.debug("Starting server");
+            try {
+                server.start();
+            } catch (Exception e) {
+//                throw new BaleenException("Unable to start server", e);
+                e.printStackTrace();
+            }
+            LOGGER.info("Server started");
+        } else {
+            System.out.println("Server has not yet been configured");
+        }
+
+        System.out.println("Started listener server");
+    }
+
+    @Override
+    protected void doGetNext(JCas jCas) throws IOException, CollectionException {
+        System.out.println("Next document is " + nextDocument);
+        DocumentAnnotation da = (DocumentAnnotation) jCas.getDocumentAnnotationFs();
+        da.setSourceUri(nextDocument.id.toString());
+        jCas.setDocumentText(nextDocument.text);
+    }
+
+    @Override
+    protected void doClose() throws IOException {
+    }
+
+    @Override
+    public synchronized boolean doHasNext() throws IOException, CollectionException {
+        nextDocument = data.poll();
+        return nextDocument != null;
+    }
+
+    public synchronized void addData(String data, Object id) {
+        this.data.add(new Document(data, id));
+    }
+
+    private class MyServlet extends HttpServlet {
+        private HttpReader collectionReader;
+
+        public MyServlet(HttpReader collectionReader) {
+            this.collectionReader = collectionReader;
+        }
+
+        //http://0.0.0.0:6413/api/1/consume
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            collectionReader.addData(req.getParameter("data"), req.getParameter("id"));
+//            resp.sendError(404);
+//            final AsyncContext asyncContext = req.startAsync();
+        }
+    }
+
+    private class Document {
+        public String text;
+        public Object id;
+
+        public Document(String text, Object id) {
+            this.text = text;
+            this.id = id;
+        }
+    }
+
+}
